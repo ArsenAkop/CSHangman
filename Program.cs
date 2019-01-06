b5794e00a2852944d3eab8df63eb5e509b92126d
Arsen A <arsenakopdzhanov@gmail.com>
2019-01-06 17:32:30 -0500

    Hangman Game

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HangMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HangmanGame game = new HangmanGame();
+            game.play();
+
+            Console.ReadLine();
+        }
+    }
+}
